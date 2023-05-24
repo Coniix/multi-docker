45a461691c19eb5f124626d98d3d068c07dd6a12
Coniix <conor-evans@hotmail.com>
2023-05-24 13:53:24 +0200

    testing app changes

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React! Change your life today
+            Learn React! Change your life today. And now we've added this!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
